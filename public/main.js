008900fc0189f81de33e51bdcad4dd09db62cdad
Alex <Alex-rld@hotmail.com>
2022-03-28 13:49:42 -0400

    Add list of pc components in html page

--- public/main.js
+++ public/main.js
@@ -1,7 +1,18 @@
 const pc_components = async () => {
    const result = await fetch('http://localhost:3000/api/pc_components')
    const data = await result.json();
-   console.log(data);
+   return data;
 }
 
-pc_components();
+const loadPcComponents = async () => {
+   const componentsList = document.getElementById('pc-components-list');
+   const btn = document.getElementById('btn-enviar');
+   const template = await pc_components();
+   const list = template.map(item => '<li>' + item.name + '</li>')//.forEach(item => componentsList.innerHTML += item);
+   componentsList.innerHTML = list.join('');
+   btn.removeAttribute('disabled');
+}
+
+window.onload = () => {
+   loadPcComponents();
+}
